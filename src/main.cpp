6a4ad2b1e89004c490c9e5d58d67fb086de70e70
Marcus Forte <marcus.forte@dunasystem.com>
2024-01-20 20:59:17 +0100

    triangle

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,13 @@
 
 #include <iostream>
 
+#include "shaderLoader.hpp"
+
+void processInput(GLFWwindow* window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
+}
+
 int main(int argc, char** argv) {
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
@@ -15,10 +22,86 @@ int main(int argc, char** argv) {
     glfwTerminate();
     return -1;
   }
+
   glfwMakeContextCurrent(window);
 
+  GLenum err = glewInit();
+  if (err != GLEW_OK) {
+    // Problem: glewInit failed, something is seriously wrong.
+    std::cout << "glewInit failed: " << glewGetErrorString(err) << std::endl;
+    exit(1);
+  }
+
+  // GL Data
+  float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f};
+
+  unsigned int VAO;
+  glGenVertexArrays(1, &VAO);
+  glBindVertexArray(VAO);
+  unsigned int VBO;
+  glGenBuffers(1, &VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+
+  shaderLoader vertex_shader("../shaders/vertex.glsl",
+                             shaderLoader::ShaderType::VERTEX);
+  auto vertexShader = vertex_shader.compile();
+  shaderLoader fragment_shader("../shaders/fragment.glsl",
+                               shaderLoader::ShaderType::FRAGMENT);
+  auto fragmentShader = fragment_shader.compile();
+
+  unsigned int shaderProgram;
+  shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+
+  int success;
+  char infoLog[512];
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+  if (!success) {
+    glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::VERTEX::PROGRAM_FAILED\n"
+              << infoLog << std::endl;
+  }
+  glUseProgram(shaderProgram);
+
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
+  //
+  std::cout << "Rendering\n";
   while (!glfwWindowShouldClose(window)) {
+    // input
+    // -----
+    processInput(window);
+
+    // render
+    // ------
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // draw our first triangle
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);  // seeing as we only have a single VAO there's no
+                             // need to bind it every time, but we'll do so to
+                             // keep things a bit more organized
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    // glBindVertexArray(0); // no need to unbind it every time
+
+    // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved
+    // etc.)
+    // -------------------------------------------------------------------------------
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
+
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
+  glDeleteProgram(shaderProgram);
+
+  glfwTerminate();
+  return 0;
 }
---
+++ src/shaderLoader.hpp
@@ -0,0 +1,54 @@
+#pragma once
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+
+#include "GL/glew.h"
+class shaderLoader {
+ public:
+  enum class ShaderType {
+    VERTEX = GL_VERTEX_SHADER,
+    FRAGMENT = GL_FRAGMENT_SHADER
+  };
+
+  shaderLoader(const std::string& path, ShaderType type) {
+    if (!std::filesystem::is_regular_file(path))
+      throw std::runtime_error("shader path is not a file: " + path);
+
+    std::string source;
+    std::ifstream file(path);
+
+    if (!file.is_open())
+      throw std::runtime_error("Unable to open shader: " + path);
+
+    std::string line;
+    while (getline(file, line)) {
+      source += line + "\n";
+    }
+
+    file.close();
+
+    const char* vertexShaderSource = source.c_str();
+
+    shaderId = glCreateShader((GLenum)type);
+    glShaderSource(shaderId, 1, &vertexShaderSource, NULL);
+    // glShaderSource()
+  }
+
+  /// Compiles and catch errors
+  unsigned int compile() const {
+    int success;
+    char infoLog[512];
+    glCompileShader(shaderId);
+    glGetShaderiv(shaderId, GL_COMPILE_STATUS, &success);
+    if (!success) {
+      glGetShaderInfoLog(shaderId, 512, NULL, infoLog);
+      throw std::runtime_error("ERROR::SHADER::COMPILATION_FAILED\n" +
+                               std::string(infoLog));
+    }
+    return shaderId;
+  }
+
+ private:
+  unsigned int shaderId;
+};
