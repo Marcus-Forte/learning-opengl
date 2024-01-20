a53c2a58ba1d4a0c2081408c2a97745b4e460585
Marcus Forte <marcus.forte@dunasystem.com>
2024-01-20 18:21:29 +0100

    first

---
+++ README.md
@@ -0,0 +1,10 @@
+# Learning OpenGL
+
+# Dependencies
+
+- Make sure your GPU drivers are installed.
+- `sudo apt install libglew-dev libglfw3-dev`
+
+# Resources
+
+- Theoretical background: https://learnopengl.com/
---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+int main(int argc, char** argv) {
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+  if (window == NULL) {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+}
